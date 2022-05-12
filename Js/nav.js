1458e0c24107305000f9802ba94307e2e58d80b2
keinnerross <keinnerross@gmail.com>
2022-05-11 20:46:10 -0400

    Update

---
+++ Js/nav.js
@@ -0,0 +1,32 @@
+/* Navegation menu trought app */
+let task = document.querySelector('.task-nav')
+let habits = document.querySelector('.habits-nav');
+let random = document.querySelector('.random-nav');
+
+
+/*sections boxes of menu*/
+let sectionTask= document.querySelector('#section-task');
+let sectionHabits= document.querySelector('#section-habits');
+let sectionRandom= document.querySelector('#section-random');
+
+/*event on click */
+
+task.addEventListener('click', () => {
+    sectionHabits.style.display= 'none'
+    sectionRandom.style.display= 'none'
+    sectionTask.style.display= 'block'
+
+});
+
+habits.addEventListener('click', () => {
+    sectionTask.style.display= 'none'
+    sectionRandom.style.display= 'none'
+    sectionHabits.style.display= 'block'
+    
+});
+
+random.addEventListener('click', () =>{
+    sectionTask.style.display= 'none'
+    sectionHabits.style.display= 'none'
+    sectionRandom.style.display= 'block'
+})
